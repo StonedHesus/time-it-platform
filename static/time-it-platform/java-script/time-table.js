7bcb784c28303d47fb9e61b70c8fb87dd5360f6a
stonedhesus <ionescu_paul@outlook.com>
2022-06-18 14:24:43 +0200

    Alter the manner in which the table is generated so that it looks as it ought to plus I added a table header element and an overflow property so as to make the time table fit nicely within a more complex UI.

--- static/time-it-platform/java-script/time-table.js
+++ static/time-it-platform/java-script/time-table.js
@@ -11,27 +11,54 @@ let containerOfTable = document.getElementById('time-table');
 // Create a new table element.
 let currentTable = document.createElement('table');
 
+// Create a new table header.
+let currentTableHeader = document.createElement('thead');
+
+// Add the table header elements to the table header.
+for(let i = 0 ; i < TABLE_COLUMNS_IDENTIFIERS.length ; ++i){
+
+    // Create a new table head element.
+    let currentTableHeadElement = document.createElement('th');
+
+    // Add to it the day which corresponds to that particular column and capitalise the first letter of it.
+    currentTableHeadElement.innerText = TABLE_COLUMNS_IDENTIFIERS[i].charAt(0).toUpperCase() +
+        TABLE_COLUMNS_IDENTIFIERS[i].slice(1);
+
+    // Append the current table head element to the table header.
+    currentTableHeader.append(currentTableHeadElement);
+}
+
+// Add the table header to the current table element.
+currentTable.append(currentTableHeader);
+
 // Create a new table body element for our table element.
 let currentTableBody = document.createElement('tbody');
 
 // Create seven columns, one for each day of the week, and twenty four rows, one for each hour of the day and
 // add them to the currentTableBody element.
-for(let columns = 0 ; columns < NUMBER_OF_COLUMNS ; ++columns){
+for(let rows = 0 ; rows < NUMBER_OF_ROWS ; ++rows){
 
     // Create a new table row element in which to store our twenty four table data elements and assigned to it an
     // unique identifier.
-    let currentColumn = document.createElement('tr');
-    currentColumn.setAttribute('id', TABLE_COLUMNS_IDENTIFIERS[columns]);
-    for(let rows = 0 ; rows < NUMBER_OF_ROWS ; ++rows){
+    let currentRow = document.createElement('tr');
+    // currentRow.setAttribute('id', TABLE_COLUMNS_IDENTIFIERS[columns]);
+    for(let column = 0 ; column < NUMBER_OF_COLUMNS ; ++column){
 
         // Instantiate a new table data element and append it to the currentColumn.
-        let currentLine = document.createElement('td');
-        currentColumn.append(currentLine);
+        let currentCell = document.createElement('td');
+        currentCell.setAttribute('class', 'time-table__block')
+        currentCell.addEventListener("click", function () {
+
+            currentCell.setAttribute('class', 'touched');
+        })
+        currentRow.append(currentCell);
     }
 
-    // Append the current column to the table.
-    currentTable.append(currentColumn)
+    currentTableBody.append(currentRow);
 }
+
+// Append the current table body to the current table.
+currentTable.append(currentTableBody);
 
 // Append the table to the current selected portion of the page.
 containerOfTable.appendChild(currentTable);

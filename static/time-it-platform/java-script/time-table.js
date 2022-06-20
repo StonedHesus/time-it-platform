1fd8639cae0de473361a4a60437eba509825ab3e
stonedhesus <ionescu_paul@outlook.com>
2022-06-20 13:20:38 +0200

    Commenced working on the ability to add a JavaScript generated modal which allows one to alter information regarding the event which the user wants to create.

--- static/time-it-platform/java-script/time-table.js
+++ static/time-it-platform/java-script/time-table.js
@@ -5,6 +5,9 @@ const NUMBER_OF_ROWS    = 24; // Each day consists of twenty four hours on our l
 // A list of unique identifier which will be used for our columns.
 const TABLE_COLUMNS_IDENTIFIERS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
+// Store a reference to the body element of the current targeted page.
+let currentBody = document.body;
+
 // Retrieve the location where the newly dynamically generated table is to be positioned.
 let containerOfTable = document.getElementById('time-table');
 
@@ -47,9 +50,14 @@ for(let rows = 0 ; rows < NUMBER_OF_ROWS ; ++rows){
         // Instantiate a new table data element and append it to the currentColumn.
         let currentCell = document.createElement('td');
         currentCell.setAttribute('class', 'time-table__block')
-        currentCell.addEventListener("click", function () {
+        currentCell.addEventListener("click", function (event) {
 
-            currentCell.setAttribute('class', 'touched');
+            currentBody.append(createEnquiryForm());
+
+            // let newCell = document.createElement('td');
+            // prompt("The current coordinates are" + event.clientY + " " + event.clientX);
+            // newCell.setAttribute('style', 'time-table__block__new-activity');
+            // newCell.append(currentRow);
         })
         currentRow.append(currentCell);
     }
@@ -62,3 +70,81 @@ currentTable.append(currentTableBody);
 
 // Append the table to the current selected portion of the page.
 containerOfTable.appendChild(currentTable);
+
+// Methods of the script.
+function addField(nameOfTheField, typeOfTheField){
+    /**
+     * @param {string} nameOfTheField; indicates the name which will be utilised both as an label and identifier for
+     * the dynamically generated field.
+     * @param {string} typeOfTheField; indicates which type of field we ought to create, i.e. whether we have a basic
+     * text input field or a multiple choices one.
+     *
+     * This here method generates a new field element and its corresponding label and it returns it to the caller in
+     * the hopes that the caller might append them further to a container or another element.
+     *
+     * @return {tuple} a two valued tuple/list structure which contains on position 0 the label and on position 1 the
+     * actual input field element.
+     *
+     * @author Andrei-Paul Ionescu.
+     */
+}
+
+function createEnquiryForm(){
+    /**
+     * @param none; this here method requires no arguments upon invocation.
+     * @type {HTMLDivElement}
+     * @returns a dynamically generated pop-up form which allows the user to customise information regarding its event/activity.
+     *
+     * This here method generates a pop-up form which based on the selection might by the user automatically suggests
+     * certain options for the event which is in the process to be created, some of which are the starting hour
+     * which is indicated thanks to the user's click event, and the end hour, for by construction we assume that
+     * it is most likely that the event will be at least one hour.
+     *
+     * @author Andrei-Paul Ionescu
+     */
+
+    // Create a new div element in which we will store the form elements.
+    let formContainer = document.createElement('div');
+
+    // Link it with the bootstrap style classes which apply to it and also the custom ones which we have created.
+    formContainer.setAttribute('class', 'container time-table__enquiry-form__container');
+
+    // Create the form element.
+    let formBody = document.createElement('form');
+
+    // Create a container for the form's inputs and buttons.
+    let formBodyContainer = document.createElement('div');
+
+    // Link the form body's container to its specific css classes.
+    formBodyContainer.setAttribute('class', 'form-group');
+
+    // Create a label for the field STARTING TIME.
+    let label = document.createElement('label');
+
+    // Set the label for attribute so that it matches with the id of the input element.
+    label.setAttribute('for', 'starting-time')
+
+    // Set the text of the label.
+    label.textContent = "Starting time"
+
+    // Append the label to the current container.
+    formBodyContainer.append(label);
+
+    // Create the From field of the form.
+    let formFieldFrom = document.createElement('input');
+
+    // Add an id to the current field so as to be able to link it with its corresponding label.
+    formFieldFrom.setAttribute('id', 'starting-time');
+
+    // Add the form field element to the form container element.
+    formBodyContainer.append(formFieldFrom);
+
+    // Add the form body container to the form element.
+    formBody.append(formBodyContainer);
+
+    // Add the form element to the current container.
+    formContainer.append(formBody);
+
+    // Finally return the newly dynamically generated pop-up form to the caller of the method.
+    return formContainer;
+}
